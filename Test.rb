8d1ebd9f9f3b43f6ae5a97ff470be2610ebdee17
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-04 09:13:46 +0100

    user imput tests

--- Test.rb
+++ Test.rb
@@ -1,2 +1,8 @@
-name = "Ben"
-puts name.upcase.reverse
+print "What's your first name?"
+first_name = gets.chomp
+print "What's your last name?"
+last_name = gets.chomp
+print "What city do you live in?"
+city = gets.chomp
+print "What state or province to do live in?"
+state = gets.chomp
