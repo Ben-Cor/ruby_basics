f4aa244f41ba0d1c56f3a612c6c4363c4141c6bf
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-05 10:43:52 +0100

    while loop practice

--- practice.rb
+++ practice.rb
@@ -1,9 +1,5 @@
-print "Pleathe enter a thtring: "
-user_input = gets.chomp
-user_input.downcase!
-
-if user_input.gsub!(/s/, "th")
-  puts"#{user_input}"
-else
-  print "no s in the string"
+i = 0
+while i < 5
+  puts i
+  i = i + 1
 end
