c9f5bcff5dac31cc4dc5f9300ed204866ac82454
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-06 09:43:55 +0100

    code cleanup and redact function init

--- practice.rb
+++ practice.rb
@@ -1,32 +1,5 @@
-i = 0
-while i < 5
-  puts i
-  i += 1
-end
+puts "Enter some text: "
+text = gets.chomp
 
-for num in 1..15
-  puts num
-end
-
-i = 20
-loop do
-  i -= 1
-  print "#{i}"
-  break if i <= 0
-end
-
-array = [1,2,3,4,5]
-
-array.each do |x|
-  x += 10
-  print "#{x}"
-end
-
-i = 30
-loop do
-  print "Ruby!"
-  i -= 1
-  break if i <= 0
-end
-
-30.times { print "Ruby!"}
+puts "Enter words to redact: "
+redact = gets.chomp
