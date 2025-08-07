605f60a0a91a8f45cbf98555e8db00647f468a70
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-07 09:37:41 +0100

    user input

--- practice.rb
+++ practice.rb
@@ -1,37 +1,6 @@
-puts "Enter some text: "
+# get user input
+puts "Enter a sentence: "
 text = gets.chomp
 
-puts "Enter words to redact: "
-redact = gets.chomp
-
-words = text.split(" ")
-
-words.each do |word|
-  if word == redact
-    print "REDACTED "
-  else
-    print "#{word} "
-  end
-end
-
-demo_array = [100, 200, 300, 400, 500]
-
-print  "#{demo_array[2]}"
-
-languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
-
-languages.each { |language| puts language }
-
-s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
-
-s.each { |ingredients| sub_array = ingredients
-sub_array.each {|item| puts item}}
-
-secret_identities = {
-  "The Batman" => "Bruce Wayne",
-  "Superman" => "Clark Kent",
-  "Wonder Woman" => "Diana Prince",
-  "Freakazoid" => "Dexter Douglas"
-}
-
-secret_identities.each { |hero, name| puts "#{hero}: #{name}"}
+# split user input
+words = text.split
