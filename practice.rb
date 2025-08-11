7e21003925520435b716bc798a00235e157a4fed
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-11 12:06:08 +0100

    symbols practice

--- practice.rb
+++ practice.rb
@@ -10,3 +10,10 @@ end
 numbers = [5, 1, 3, 8]
 
 puts alphabetize(numbers)
+
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+symbols = []
+
+strings.each {|s| symbols.push(s.to_sym)}
+print symbols
