86ea35618249bd39f5bbfe4a38fbcb26d32e55e6
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-11 10:55:47 +0100

    sort array function

--- practice.rb
+++ practice.rb
@@ -1,30 +1,12 @@
-# get user input
-puts "Enter a sentence: "
-text = gets.chomp
-
-# split user input
-words = text.split
-
-#hash with default value
-frequencies = Hash.new(0)
-
-# add words to the hash and add value by one
-words.each do |word|
-  word = word.downcase
-  frequencies["#{word}"] += 1
+def alphabetize(arr, rev=false)
+  arr.sort!
+  if rev
+    arr.reverse!
+  else
+    return arr
+  end
 end
 
-# sort frequencies
-frequencies = frequencies.sort_by do |word, count|
-  count
-end
-frequencies.reverse
+numbers = [5, 1, 3, 8]
 
-# display hash
-frequencies.each { |word, count| puts word + " " + count.to_s}
-
-def what_up(greeting, *friends)
-  friends.each { |friend| puts "#{greeting}, #{friend}!" }
-end
-
-what_up("What up", "Ian", "Zoe", "Zenas", "Eleanor")
+puts alphabetize(numbers)
