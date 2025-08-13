bac54771cf91db0b962a8394698aa94c8d8baae1
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-08-13 12:13:53 +0100

    movies simple programme

--- practice.rb
+++ practice.rb
@@ -1,19 +1,43 @@
-def alphabetize(arr, rev=false)
-  arr.sort!
-  if rev
-    arr.reverse!
+movies = {
+  Up: 4,
+  Memento: 4
+}
+
+puts "What would you like to do?"
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "What movie would you like to add?"
+  title = gets.chomp
+  puts "What os the movie rating (1-4)?"
+  rating = gets.chomp
+  if movies[title.to_sym] == nil
+    movies[title.to_sym] = rating.to_i
+    puts "The movie and rating has been added"
   else
-    return arr
+    puts"This movie already exists in the database"
   end
+when "update"
+  puts "What is the movie title?"
+  title = gets.chomp
+  if movies[title] == nil
+    puts "This movie is not in the database"
+  else
+    puts "What is the rating?"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  movies.each {|movie, rating| puts "#{movie}: #{rating}"}
+when "delete"
+  puts "What is the movie title?"
+  title = gets.chomp
+  if movies[title] == nil
+    puts "The movie is not in the database"
+  else
+    movies.delete(title.to_sym)
+  end
+else
+  puts "Error!"
 end
-
-numbers = [5, 1, 3, 8]
-
-puts alphabetize(numbers)
-
-strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
-
-symbols = []
-
-strings.each {|s| symbols.push(s.to_sym)}
-print symbols
